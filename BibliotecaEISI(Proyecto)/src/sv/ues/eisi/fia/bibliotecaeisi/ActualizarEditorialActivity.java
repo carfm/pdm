5c629c3bf5376e675c598acd843c9abd7f2cb14b
ANIBAL <ANIBAL@ANIBAL-PC.lan>
2014-06-20 01:08:46 -0600

    puse el codigo para glassfish pero no puedo probar

--- BibliotecaEISI(Proyecto)/src/sv/ues/eisi/fia/bibliotecaeisi/ActualizarEditorialActivity.java
+++ BibliotecaEISI(Proyecto)/src/sv/ues/eisi/fia/bibliotecaeisi/ActualizarEditorialActivity.java
@@ -1,19 +1,24 @@
 package sv.ues.eisi.fia.bibliotecaeisi;
 
+import sv.ues.eisi.fia.bibliotecaeisi.clases.Documento;
 import sv.ues.eisi.fia.bibliotecaeisi.clases.Editorial;
+import sv.ues.eisi.fia.bibliotecaeisi.clases.Pais;
 import sv.ues.eisi.fia.bibliotecaeisi.controlbase.ControlBaseDatos;
+import sv.ues.eisi.fia.bibliotecaeisi.controlwebservice.ControladorWebService;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class ActualizarEditorialActivity extends Activity {
-
+	ControlBaseDatos BDhelper;
 	ControlBaseDatos helper;
 	EditText editNombreEditorial;
 	EditText editId;
+	Button butActualizarEditorial;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,7 @@ public class ActualizarEditorialActivity extends Activity {
 		return true;
 	}
 
+	
 	public void actualizarEditorial(View v) {
 		Editorial e = new Editorial();
 		e.setIdEditorial(Integer.parseInt(editId.getText().toString()));
@@ -38,6 +44,19 @@ public class ActualizarEditorialActivity extends Activity {
 		helper.abrir();
 		String regInsertados = helper.actualizar(e);
 		helper.cerrar();
+		
+/*		Gson gson = new Gson();
+        //MiObjeto obj = new MiObjeto("Juan", "Madrid", null);
+        String jsonString = gson.toJson(new Pais("ESA","El Salvador"));
+        System.out.println("JSON: " + jsonString);*/
+        ControladorWebService parser = new ControladorWebService();
+        try {
+			parser.sendHttpRequest(e,"sv.fia.ues.bibliotecaeisi.entidad.editorial","POST");
+			//salidaHost.setText("se realizo correctamente ");
+		} catch (Exception ee) {
+			// TODO Auto-generated catch block
+			//salidaHost.setText(ControladorWebService.informacionError);
+		}
 		Toast.makeText(this, regInsertados, Toast.LENGTH_SHORT).show();
 	}
 
